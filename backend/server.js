b505d20bbfc903f2f9af821d6587d6638229b036
anadev29 <anasanchezrivera62@gmail.com>
2025-11-05 16:56:58 -0500

    Correccion de importacion

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
-import { testConnection } from "../backend/database/connectiondb.js";
+import { testConnection } from "./database/connectiondb.js";
 
 const PORT = process.env.PORT || 3000;
 
